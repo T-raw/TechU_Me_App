b60e0feb7d9f9937f01f00d5987ffa4ea735372e
James Plant <james.plant@ocsbstudent.ca>
2013-12-03 11:13:22 -0500

    Changes made to the layout by Red Moon

--- FocusApp/gen/com/example/focusapp/R.java
+++ FocusApp/gen/com/example/focusapp/R.java
@@ -24,16 +24,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int LinearLayout1=0x7f080000;
+        public static final int LinearLayout1=0x7f080001;
+        public static final int LinearLayout2=0x7f080000;
         public static final int action_settings=0x7f080009;
-        public static final int button1=0x7f080001;
-        public static final int button2=0x7f080002;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080003;
-        public static final int button5=0x7f080006;
-        public static final int button6=0x7f080005;
-        public static final int button7=0x7f080008;
-        public static final int button8=0x7f080007;
+        public static final int button1=0x7f080002;
+        public static final int button2=0x7f080003;
+        public static final int button3=0x7f080005;
+        public static final int button4=0x7f080006;
+        public static final int button5=0x7f080007;
+        public static final int button6=0x7f080008;
+        public static final int button7=0x7f080004;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -42,9 +42,18 @@ public final class R {
         public static final int main=0x7f070000;
     }
     public static final class string {
+        public static final int BLT=0x7f050006;
+        public static final int Bacon=0x7f050005;
+        public static final int Cbsandwich=0x7f05000a;
+        public static final int Sauceage=0x7f050008;
+        public static final int Sausuage=0x7f050009;
+        public static final int Toast=0x7f050007;
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int eggs=0x7f050004;
+        public static final int finfries=0x7f05000b;
         public static final int hello_world=0x7f050002;
+        public static final int yeeee=0x7f050003;
     }
     public static final class style {
         /** 
