4775686bfe8f9596763cccaac38e85bf29a08b5d
kingsley Akorjuru <kjakorjuru@gmail.com>
2013-12-04 10:43:39 -0500

    My last changes

--- FocusApp/gen/com/example/focusapp/R.java
+++ FocusApp/gen/com/example/focusapp/R.java
@@ -25,11 +25,16 @@ public final class R {
     }
     public static final class id {
         public static final int LinearLayout2=0x7f080000;
-        public static final int action_settings=0x7f080005;
+        public static final int action_settings=0x7f08000a;
         public static final int button1=0x7f080002;
         public static final int button2=0x7f080003;
-        public static final int button3=0x7f080001;
-        public static final int button4=0x7f080004;
+        public static final int button3=0x7f080004;
+        public static final int button4=0x7f080005;
+        public static final int button5=0x7f080006;
+        public static final int button6=0x7f080007;
+        public static final int button7=0x7f080008;
+        public static final int button8=0x7f080009;
+        public static final int searchView1=0x7f080001;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
