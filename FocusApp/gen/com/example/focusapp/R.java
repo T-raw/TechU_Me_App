7c91bbb6e6a306db3fd2a316d9bb28ea03944adf
kingsley Akorjuru <kjakorjuru@gmail.com>
2013-12-10 11:00:08 -0500

    Changed it to a tablet layout, also added image buttons instead of regular buttons

--- FocusApp/gen/com/example/focusapp/R.java
+++ FocusApp/gen/com/example/focusapp/R.java
@@ -9,6 +9,17 @@ package com.example.focusapp;
 
 public final class R {
     public static final class attr {
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarButtonStyle=0x7f010001;
+        /** <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+         */
+        public static final int buttonBarStyle=0x7f010000;
+    }
+    public static final class color {
+        public static final int black_overlay=0x7f040000;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -17,121 +28,105 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int bacon=0x7f020000;
+        public static final int burger=0x7f020001;
+        public static final int clubsandwich=0x7f020002;
+        public static final int coffee=0x7f020003;
+        public static final int eggs=0x7f020004;
+        public static final int focus=0x7f020005;
+        public static final int focus123=0x7f020006;
+        public static final int fried_rice=0x7f020007;
+        public static final int friez=0x7f020008;
+        public static final int ic_launcher=0x7f020009;
+        public static final int lunchsandwhich=0x7f02000a;
+        public static final int pizza=0x7f02000b;
+        public static final int sausage=0x7f02000c;
+        public static final int soda=0x7f02000d;
+        public static final int toast=0x7f02000e;
     }
     public static final class id {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        public static final int LinearLayout2=0x7f080000;
-        public static final int action_settings=0x7f08000a;
-        public static final int button1=0x7f080002;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080004;
-        public static final int button4=0x7f080005;
-        public static final int button5=0x7f080006;
-        public static final int button6=0x7f080007;
-        public static final int button7=0x7f080008;
-        public static final int button8=0x7f080009;
-        public static final int searchView1=0x7f080001;
-=======
-        public static final int LinearLayout1=0x7f080001;
-        public static final int LinearLayout2=0x7f080000;
-        public static final int action_settings=0x7f080017;
-        public static final int button1=0x7f080002;
-        public static final int button10=0x7f08000c;
-        public static final int button11=0x7f08000f;
-        public static final int button12=0x7f080010;
-        public static final int button13=0x7f080011;
-        public static final int button14=0x7f080013;
-        public static final int button15=0x7f080014;
-        public static final int button16=0x7f080015;
-        public static final int button2=0x7f080003;
-        public static final int button3=0x7f080005;
-        public static final int button4=0x7f080006;
-        public static final int button5=0x7f080008;
-        public static final int button6=0x7f080009;
-        public static final int button7=0x7f080004;
-        public static final int button8=0x7f08000d;
-        public static final int button9=0x7f08000b;
-        public static final int textView2=0x7f080007;
-        public static final int textView3=0x7f08000a;
-        public static final int textView4=0x7f08000e;
-        public static final int textView5=0x7f080012;
-        public static final int textView6=0x7f080016;
->>>>>>> branch 'master' of https://github.com/T-raw/TechU_Me_App.git
-=======
-        public static final int LinearLayout1=0x7f080000;
-        public static final int LinearLayout2=0x7f080001;
-        public static final int action_settings=0x7f08001c;
-        public static final int button1=0x7f080008;
-        public static final int button10=0x7f080011;
-        public static final int button11=0x7f080014;
-        public static final int button12=0x7f080015;
-        public static final int button13=0x7f080016;
-        public static final int button14=0x7f080018;
-        public static final int button15=0x7f080019;
-        public static final int button16=0x7f08001a;
-        public static final int button2=0x7f080009;
-        public static final int button3=0x7f08000b;
-        public static final int button4=0x7f08000c;
-        public static final int button5=0x7f08000d;
-        public static final int button6=0x7f08000e;
-        public static final int button7=0x7f08000a;
-        public static final int button8=0x7f080012;
-        public static final int button9=0x7f080010;
-        public static final int editText1=0x7f080002;
-        public static final int editText2=0x7f080003;
-        public static final int editText3=0x7f080005;
-        public static final int editText4=0x7f080007;
-        public static final int textView1=0x7f080006;
-        public static final int textView2=0x7f080004;
-        public static final int textView3=0x7f08000f;
-        public static final int textView4=0x7f080013;
-        public static final int textView5=0x7f080017;
-        public static final int textView6=0x7f08001b;
->>>>>>> branch 'master' of https://github.com/T-raw/TechU_Me_App.git
+        public static final int LinearLayout1=0x7f090000;
+        public static final int LinearLayout2=0x7f090001;
+        public static final int action_settings=0x7f090020;
+        public static final int button1=0x7f09000b;
+        public static final int button10=0x7f090011;
+        public static final int button11=0x7f090014;
+        public static final int button12=0x7f090015;
+        public static final int button13=0x7f090016;
+        public static final int button14=0x7f090018;
+        public static final int button15=0x7f090019;
+        public static final int button16=0x7f09001a;
+        public static final int button2=0x7f09000c;
+        public static final int button3=0x7f090008;
+        public static final int button4=0x7f09000a;
+        public static final int button5=0x7f09000d;
+        public static final int button6=0x7f09000e;
+        public static final int button7=0x7f090009;
+        public static final int button8=0x7f090012;
+        public static final int button9=0x7f090010;
+        public static final int dummy_button=0x7f09001e;
+        public static final int editText1=0x7f090002;
+        public static final int editText2=0x7f090003;
+        public static final int editText3=0x7f090005;
+        public static final int editText4=0x7f090007;
+        public static final int fullscreen_content=0x7f09001c;
+        public static final int fullscreen_content_controls=0x7f09001d;
+        public static final int imageView1=0x7f09001f;
+        public static final int textView1=0x7f090006;
+        public static final int textView2=0x7f090004;
+        public static final int textView3=0x7f09000f;
+        public static final int textView4=0x7f090013;
+        public static final int textView5=0x7f090017;
+        public static final int textView6=0x7f09001b;
     }
     public static final class layout {
         public static final int activity_done_menu=0x7f030000;
         public static final int activity_main=0x7f030001;
         public static final int activity_settings=0x7f030002;
+        public static final int activity_splash_screen=0x7f030003;
+        public static final int button=0x7f030004;
     }
     public static final class menu {
-        public static final int done_menu=0x7f070000;
-        public static final int main=0x7f070001;
-        public static final int settings=0x7f070002;
+        public static final int done_menu=0x7f080000;
+        public static final int main=0x7f080001;
+        public static final int settings=0x7f080002;
     }
     public static final class string {
-        public static final int BLT=0x7f050006;
-        public static final int Bacon=0x7f050005;
-        public static final int Cbsandwich=0x7f05000a;
-        public static final int Items=0x7f050015;
-        public static final int Pricees=0x7f050016;
-        public static final int Sauceage=0x7f050008;
-        public static final int Sausuage=0x7f050009;
-        public static final int Toast=0x7f050007;
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int brownie=0x7f050011;
-        public static final int cake=0x7f05000f;
-        public static final int chknwngz=0x7f05000c;
-        public static final int eggs=0x7f050004;
-        public static final int finfries=0x7f05000b;
-        public static final int hamburger=0x7f05000d;
-        public static final int hello_world=0x7f050002;
-        public static final int icecream=0x7f050010;
-        public static final int nachos=0x7f050012;
-        public static final int rice=0x7f05000e;
-        public static final int shrimp=0x7f050013;
-        public static final int subtotal=0x7f050017;
-        public static final int tax=0x7f050018;
-        public static final int title_activity_done_menu=0x7f050014;
-        public static final int title_activity_settings=0x7f050019;
-        public static final int yeeee=0x7f050003;
+        public static final int BLT=0x7f060006;
+        public static final int Bacon=0x7f060005;
+        public static final int Breakfast=0x7f06001e;
+        public static final int Cbsandwich=0x7f06000a;
+        public static final int Items=0x7f060015;
+        public static final int Pricees=0x7f060016;
+        public static final int Sauceage=0x7f060008;
+        public static final int Sausuage=0x7f060009;
+        public static final int Toast=0x7f060007;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int breakfast=0x7f06001d;
+        public static final int brownie=0x7f060011;
+        public static final int cake=0x7f06000f;
+        public static final int chknwngz=0x7f06000c;
+        public static final int dummy_button=0x7f06001b;
+        public static final int dummy_content=0x7f06001c;
+        public static final int eggs=0x7f060004;
+        public static final int finfries=0x7f06000b;
+        public static final int hamburger=0x7f06000d;
+        public static final int hello_world=0x7f060002;
+        public static final int icecream=0x7f060010;
+        public static final int nachos=0x7f060012;
+        public static final int rice=0x7f06000e;
+        public static final int shrimp=0x7f060013;
+        public static final int subtotal=0x7f060017;
+        public static final int tax=0x7f060018;
+        public static final int title_activity_done_menu=0x7f060014;
+        public static final int title_activity_settings=0x7f060019;
+        public static final int title_activity_splash_screen=0x7f06001a;
+        public static final int yeeee=0x7f060003;
     }
     public static final class style {
         /** 
@@ -155,10 +150,56 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
+        public static final int ButtonBar=0x7f070003;
+        public static final int ButtonBarButton=0x7f070004;
+        public static final int FullscreenActionBarStyle=0x7f070005;
+        public static final int FullscreenTheme=0x7f070002;
     }
+    public static final class styleable {
+        /** 
+         Declare custom theme attributes that allow changing which styles are
+         used for button bars depending on the API level.
+         ?android:attr/buttonBarStyle is new as of API 11 so this is
+         necessary to support previous API levels.
+    
+           <p>Includes the following attributes:</p>
+           <table>
+           <colgroup align="left" />
+           <colgroup align="left" />
+           <tr><th>Attribute</th><th>Description</th></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarButtonStyle com.example.focusapp:buttonBarButtonStyle}</code></td><td></td></tr>
+           <tr><td><code>{@link #ButtonBarContainerTheme_buttonBarStyle com.example.focusapp:buttonBarStyle}</code></td><td></td></tr>
+           </table>
+           @see #ButtonBarContainerTheme_buttonBarButtonStyle
+           @see #ButtonBarContainerTheme_buttonBarStyle
+         */
+        public static final int[] ButtonBarContainerTheme = {
+            0x7f010000, 0x7f010001
+        };
+        /**
+          <p>This symbol is the offset where the {@link com.example.focusapp.R.attr#buttonBarButtonStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.example.focusapp:buttonBarButtonStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarButtonStyle = 1;
+        /**
+          <p>This symbol is the offset where the {@link com.example.focusapp.R.attr#buttonBarStyle}
+          attribute's value can be found in the {@link #ButtonBarContainerTheme} array.
+
+
+          <p>Must be a reference to another resource, in the form "<code>@[+][<i>package</i>:]<i>type</i>:<i>name</i></code>"
+or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>name</i></code>".
+          @attr name com.example.focusapp:buttonBarStyle
+        */
+        public static final int ButtonBarContainerTheme_buttonBarStyle = 0;
+    };
 }
